e5aa5e1ccf8b4f0a38d3b9175e94ec78f51f304e
Priyanshu <priyanshusamanta101@gmail.com>
2026-02-06 14:22:45 +0530

    Add Subscirption

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Box, Layers, ChevronDown, LibraryBig, Users, ShoppingCart, Truck, BadgeCheck } from "lucide-react";
+import { Box, Layers, ChevronDown, LibraryBig, Users, ShoppingCart, Truck, BadgeCheck,BadgeIndianRupee } from "lucide-react";
 
 export default function Sidebar() {
   const pathname = usePathname() ?? "";
@@ -140,6 +140,21 @@ export default function Sidebar() {
         >
           <BadgeCheck size={18} />
           Subscription
+        </Link>
+
+        {/* Subscription (NO SUBMENU) */}
+        <Link
+          href="/admin/payment/PaymentPage"
+          className={`mt-1 flex items-center gap-3 rounded-xl px-4 py-3 transition
+            ${
+              pathname.startsWith("/admin/payment")
+                ? "bg-blue-50 text-blue-600 font-medium"
+                : "text-gray-700 hover:bg-gray-100"
+            }
+          `}
+        >
+          <BadgeIndianRupee size={18} />
+          Payment
         </Link>
 
 
---
+++ pages/admin/payment/PaymentPage.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
+import PaymentTable from "@/components/payment/PaymentTable";
+
+export default function PaymentPage() {
+  return (
+    <div className="p-6">
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-1 flex-col">
+            <Header />
+            <div className="p-6">
+                <PaymentTable />
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
